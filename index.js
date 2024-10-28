1f439c52daccf54ce2456bfd329136e1f65caddf
Sliferyfire <sliferyfire@gmail.com>
2024-10-28 00:16:40 -0600

    prueba vercel

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express=require("express");
 var rutas=require("./routes/citasRutas");
+var path = require('path');
  
 var app=express();
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
